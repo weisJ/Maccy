f3221ffecab0ee00b31b6d3e0fe5b97e3f3c9297
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2024-08-03 16:31:19 +0200

    Custom Popover

--- Maccy/Views/ContentView.swift
+++ Maccy/Views/ContentView.swift
@@ -1,6 +1,70 @@
 import SwiftData
 import SwiftUI
 
+struct PopoverView<Value, T: View>: NSViewRepresentable {
+  @Binding private var item: Value?
+  private let content: (Value?) -> T
+  private var lastItem: Value? = nil
+
+  init(item: Binding<Value?>, @ViewBuilder content: @escaping (Value?) -> T) {
+    self._item = item
+    self.content = content
+  }
+
+  func makeNSView(context: Context) -> NSView {
+    return .init()
+  }
+
+  func updateNSView(_ nsView: NSView, context: Context) {
+    let coordinator = context.coordinator
+    let currentItem = item
+    coordinator.popover.contentViewController = NSHostingController(rootView: content(currentItem ?? coordinator.lastItem))
+    coordinator.lastItem = currentItem
+    coordinator.visibilityDidChange(currentItem != nil, in: nsView)
+  }
+
+  func makeCoordinator() -> Coordinator {
+    let coordinator = Coordinator(popover: .init())
+    coordinator.popover.contentViewController = NSHostingController(rootView: content(nil))
+    return coordinator
+  }
+
+  @MainActor
+  final class Coordinator: NSObject, NSPopoverDelegate {
+    fileprivate let popover: NSPopover
+    fileprivate var lastItem: Value? = nil
+    var oldFrame: NSRect = .zero
+
+
+    fileprivate init(popover: NSPopover) {
+      self.popover = popover
+      super.init()
+      popover.delegate = self
+
+      // Prevent NSPopover from becoming first responder.
+      popover.behavior = .semitransient
+    }
+
+    fileprivate func visibilityDidChange(_ isVisible: Bool, in view: NSView) {
+      if isVisible {
+        if oldFrame == .zero {
+          oldFrame = view.frame
+        } else {
+          view.frame = oldFrame
+        }
+
+        popover.show(relativeTo: .zero, of: view, preferredEdge: .maxX)
+        // Ugly hack to hide the arrow
+        view.frame = NSMakeRect(-1000, 0, 10, 10)
+
+      } else if popover.isShown {
+        popover.close()
+      }
+    }
+  }
+}
+
+
 struct ContentView: View {
   @State private var appState = AppState.shared
   @State private var modifierFlags = ModifierFlags()
@@ -37,6 +101,13 @@ struct ContentView: View {
       .onMouseMove {
         appState.isKeyboardNavigating = false
       }
+      .background {
+        PopoverView(item: $appState.previewItem) { content in
+          if let item = content {
+            PreviewItemView(item: item)
+          }
+        }
+      }
       .task {
         try? await appState.history.load()
       }
@@ -59,13 +130,9 @@ struct ContentView: View {
         scenePhase = .background
       }
     }
-    .onReceive(NotificationCenter.default.publisher(for: NSPopover.willShowNotification)) {
-      if let popover = $0.object as? NSPopover {
-        // Prevent NSPopover from showing close animation when
-        // quickly toggling FloatingPanel while popover is visible.
-        popover.animates = false
-        // Prevent NSPopover from becoming first responder.
-        popover.behavior = .semitransient
+    .onReceive(NotificationCenter.default.publisher(for: NSPopover.didCloseNotification)) {
+      if $0.object is NSPopover {
+        appState.showPreview = false
       }
     }
   }
